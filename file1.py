edfe05f24a93e0e3a3a234d783765b9e8e47d70e
vaitheeshm <157804244+vaitheeshm@users.noreply.github.com>
2024-02-28 10:17:05 +0530

    expansion panel

---
+++ file1.py
@@ -0,0 +1,9 @@
+MDExpansionPanel(
+icon="icon.png",
+content=Content(), # content of panel
+panel_cls=MDExpansionPanelThreeLine( # content of header
+text="Text",
+secondary_text="Secondary text",
+tertiary_text="Tertiary text",
+)
+)
