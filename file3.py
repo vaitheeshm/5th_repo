f963aafb4332993be7e7bd0d6fce559d5c24e994
vaitheeshm <157804244+vaitheeshm@users.noreply.github.com>
2024-02-28 10:58:45 +0530

    items were added

---
+++ file3.py
@@ -0,0 +1,8 @@
+menu_items = [
+{
+"text": "Strikethrough",
+"leading_icon": "check",
+"trailing_icon": "apple-keyboard-command",
+"trailing_text": "+Shift+X",
+"leading_icon_color": "orange",
+"trailing_icon_color": "green",
